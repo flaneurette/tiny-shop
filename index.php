de601ef0115547b62ab79cc1ce0e5a09734520cc
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2020-06-10 23:20:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,27 @@
 	/**
 		display products in the shop.
 	**/
+
+	ini_set('display_errors', 1); 
+	ini_set('session.cookie_httponly', 1);
+	ini_set('session.use_only_cookies', 1);
+	ini_set('session.cookie_secure', 1);
 	
+	error_reporting(E_ALL);
+
+	// Optional headers to consider.
+	header("X-Frame-Options: DENY"); 
+	header("X-XSS-Protection: 1; mode=block"); 
+	header("Strict-Transport-Security: max-age=30");
+	header("Referrer-Policy: same-origin");
+
+
 	error_reporting(E_ALL);
 	session_start();
 	
 	include("class.Shop.php");
 	$shop  = new Shop();
 	$shoplist = $shop->decode();
-	
 ?>
 <html>
 	<head>
@@ -65,7 +78,7 @@
 					echo '</table>';
 
 				} else {
-					echo "<p class='book'><em>Shop is empty...</em></p>";
+					echo "<p class='result'><em>Shop is empty...</em></p>";
 				}
 			?>
 			</div>
