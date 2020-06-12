850407ac046281bd9d9f0896f9023479b6a5cd17
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2020-06-12 18:20:00 +0200

    Add files via upload

---
+++ class.Session.php
@@ -0,0 +1,103 @@
+<?php
+
+
+class Session {
+
+	CONST PWD 		= "Password to encrypt session data";
+	CONST FILE_ENC  = "UTF-8";
+
+	public function __construct() {
+		$incomplete = false;
+	}
+	
+	/**
+	* Sanitizes user-input
+	* @param string
+	* @return string
+	*/
+	public function cleanInput($string) 
+	{
+		return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+	}
+	
+	/**
+	* Session array with messages
+	* @return mixed object/array
+	*/	
+	public function message($value) 
+	{
+		if(isset($_SESSION['messages'])) { 
+			array_push($_SESSION['messages'],$value);  
+			} else { 
+			$_SESSION['messages'] = array(); 
+		} 	
+	}
+	
+	/**
+	* Showing session messages.
+	* @return mixed object/array
+	*/	
+	public function showmessage() 
+	{ 
+		if(isset($_SESSION['messages'])) { 
+			echo "<pre>"; 
+			echo "<strong>Message:</strong>\r\n"; 
+			foreach($_SESSION['messages'] as $message) { 
+				echo $message . "\r\n" ; 
+			} echo "</pre>"; 
+		} 
+		$_SESSION['messages'] = array();
+	} 
+	
+	/**
+	* Encryption function (requires OpenSSL)
+	* @param string $plaintext
+	* @return $ciphertext
+	*/	
+	public function encrypt($plaintext) 
+	{
+
+		if (!function_exists('openssl_encrypt')) {
+			$this->message('Encryption failed: OpenSSL is not supported or enabled on this PHP instance.');
+			return false;
+    	}
+		
+		$key = self::PWD; // Password is set above at the Constants
+		$ivlen = openssl_cipher_iv_length($cipher="AES-256-CTR");
+		$iv = openssl_random_pseudo_bytes($ivlen);
+		$ciphertext_raw = openssl_encrypt($plaintext, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+		$hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+		$ciphertext = base64_encode($iv.$hmac.$ciphertext_raw );
+		return bin2hex($ciphertext);
+	}
+	
+	/**
+	* Decryption function (requires OpenSSL)
+	* @param string $ciphertext
+	* @return $plaintext or false if there is no support for OpenSSL.
+	*/		
+	public function decrypt($ciphertext) 
+	{
+		
+		if (!function_exists('openssl_decrypt')) {
+			$this->message('Decryption failed: OpenSSL is not supported or enabled on this PHP instance.');
+			return false;
+    	}
+		
+		$key = self::PWD; // Password is set above at the Constants
+		$ciphertext = hex2bin($ciphertext);
+		$c = base64_decode($ciphertext);
+		$ivlen = openssl_cipher_iv_length($cipher="AES-256-CTR");
+		$iv = substr($c, 0, $ivlen);
+		$hmac = substr($c, $ivlen, $sha2len=32);
+		$ciphertext_raw = substr($c, $ivlen+$sha2len);
+		$original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+		$calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+		
+		if (hash_equals($hmac, $calcmac)) { //PHP 5.6+ timing attack safe comparison
+			return $original_plaintext;
+		}
+	}
+	
+}
+?>
--- index.php
+++ index.php
@@ -1,25 +1,8 @@
 <?php
 
-	/**
-		display products in the shop.
-	**/
-
-	ini_set('display_errors', 1); 
-	ini_set('session.cookie_httponly', 1);
-	ini_set('session.use_only_cookies', 1);
-	ini_set('session.cookie_secure', 1);
-	
-	error_reporting(E_ALL);
-
-	// Optional headers to consider.
-	header("X-Frame-Options: DENY"); 
-	header("X-XSS-Protection: 1; mode=block"); 
-	header("Strict-Transport-Security: max-age=30");
-	header("Referrer-Policy: same-origin");
-
-	session_start();
-	
+	include("resources/php/header.inc.php");
 	include("class.Shop.php");
+	
 	$shop  = new Shop();
 	$shoplist = $shop->decode();
 ?>
@@ -30,58 +13,14 @@
 	</head>
 	<body>
 		<h1>Shop product list</h1>
-		<div id="shop">
-		
-			<?php 
+			<div id="shop">
 			
-				$shop  = new Shop();
-				$product_list = $shop->decode();
-
-				if($product_list !== null) {
-
-					$shoplist = $product_list;
-					$iv = array();
-					$i = 0;
-					
-						foreach($product_list as $c) {	
-							array_push($iv,$c);
-							$shop->cleanInput($c['product.title']);
-							$i++;
-						}
-					
-					echo '<table border="0" cellpadding="3" cellspacing="5" width="100%">';
-					echo '<tr><th>Status</th><th>Price</th><th>Product</th><th>Description</th><th>Category</th><th>Buy</th></tr>';
-					
-					$i = count($iv)-1;
-					
-					if($i >= 0) { 
-						while($i >= 0) {
-							if($iv[$i]['stock'] < 1) {
-								$status_color = 'status-red'; // low stock
-								} else {
-								$status_color = 'status-green';
-							}
-							echo "<tr><td width=\"90\">";
-							echo "<div class=".$status_color.">".$iv[$i]['product.status']."</div></td>";
-							echo "<td>".$shop->CURRENCIES[3][0][0].' '.$iv[$i]['product.price']."</td>";
-							echo "<td><a href=\"".$shop->seoUrl($iv[$i]['product.category']).'/'.$shop->seoUrl($iv[$i]['product.title']).'/'.$shop->cleanInput($iv[$i]['product.id'])."/\">".$shop->cleanInput($iv[$i]['product.title'])."</a> </td>";
-							echo "<td>".$iv[$i]['product.description']."</td>";
-							echo "<td>".$iv[$i]['product.category']."</td>";
-							echo "<td><input type='button' name='add_cart' value='Add to cart' /></td>";
-							echo "</tr>";
-						$i--;
-						}
-					}
-					
-					echo '</table>';
-
-				} else {
-					echo "<p class='result'><em>Shop is empty...</em></p>";
-				}
-			?>
+				<?php 
+				
+					$shop  = new Shop();
+					$products = $shop->getproducts('list');
+					echo $products;
+				?>
 			</div>
-		
-		<div id="output"></div>
-		
 	</body>
 </html>
