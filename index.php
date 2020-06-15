2cd039fbe6d9d7b08c2ce49146191ec53eb61e13
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2020-06-15 15:33:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,32 @@
 <?php
+
 	include("resources/php/header.inc.php");
 	include("class.Shop.php");
 	
 	$shop  = new Shop();
-	$products = $shop->getproducts('list',$category='index');
+	
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-	<link rel="stylesheet" type="text/css" href="resources/reset.css">
-	<link rel="stylesheet" type="text/css" href="resources/style.css">
+	<?php
+	echo $shop->getmeta();				
+	?>
 	</head>
 	<body>
 		<h1>Shop product list</h1>
 			<div id="shop">
-				<?php 
+			
+				<?php
+				$products = $shop->getproducts('list',$category='index');				
 				echo $products;
 				?>
 			</div>
 			<div id="ts.paginate">
 				<center>
-				<?php 
-				echo $shop->paginate(1);
-				?>
+					<?php 
+						echo $shop->paginate(1);
+					?>
 				</center>
 			</div>
 	</body>
