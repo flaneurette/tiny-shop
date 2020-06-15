2586aa38e3249011fd61a2c2755e7c384500259f
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2020-06-15 13:38:28 +0200

    Update cart.php

--- cart.php
+++ cart.php
@@ -4,7 +4,7 @@
 	
 	$shop = new Shop();
 ?>
-
+<!DOCTYPE html>
 <html>
 	<head>
 	<link rel="stylesheet" type="text/css" href="resources/reset.css">
@@ -20,28 +20,24 @@
 			<select name="payment_gateway" id="ts-form-cart-payment-gateway-select">
 			<option value="">Select payment method...</option>
 			<?php
-			
 			// dynamically generate payment gateways from site.json
 			$siteconf = $shop->load_json("inventory/site.json");
 			$keys = 'site.payment.gateways';
 			echo $shop->gatewaylist($siteconf,$keys);
-			
 			?>
 			</select>
 			
 		<div class="ts-shop-form-field">
 		<?php
-		
 			// dynamically generate form fields from customer.json.
 			$ignore = ['customer.id','customer.diff','customer.ua','customer.signup.ua','customer.hash','customer.signup.date','customer.signup.ip'];
-			$split = 6;
+			$split = 6; 
 			$shopconf = $shop->load_json("inventory/customer.json");
 			echo $shop->generatecart($shopconf,$split,$ignore);
-				
 		?>
 		</div>
-			<hr />
-			<input type="submit" name="submit" value="Submit">
+
+		<input type="submit" name="submit" value="Submit">
 		</form>
 		</div>
 	</body>
