54a4e4fcfcd99235ebfc2cd8d4742ee0a815ee72
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2020-06-15 02:45:37 +0200

    Update cart.php

--- cart.php
+++ cart.php
@@ -4,6 +4,7 @@
 	
 	$shop     = new Shop();
 	$shopconf = $shop->load_json("inventory/customer.json");
+	$siteconf = $shop->load_json("inventory/site.json");
 ?>
 
 <html>
@@ -19,38 +20,22 @@
 		<form name="" method="" id="ts-shop-cart-form-data">
 		<hr />
 			<select name="payment_gateway" id="ts-form-cart-payment-gateway-select">
-				<option value="">Select payment method...</option>
-				<option value="ACH">ACH</option>
-				<option value="Alipay">Alipay</option>
-				<option value="Apple Pay">Apple Pay</option>
-				<option value="Bancontact">Bancontact</option>
-				<option value="BenefitPay">BenefitPay</option>
-				<option value="Boleto Bancário">Boleto Bancário</option>
-				<option value="CitrusPay">Citrus Pay</option>
-				<option value="EPS">EPS</option>
-				<option value="Fawry">Fawry</option>
-				<option value="Giropay">Giropay</option>
-				<option value="Google Pay">Google Pay</option>
-				<option value="PayPal">PayPal</option>
-				<option value="KNET">KNET</option>
-				<option value="Klarna">Klarna</option>
-				<option value="Mada">Mada</option>
-				<option value="Multibanco">Multibanco</option>
-				<option value="OXXO">OXXO</option>
-				<option value="Pago Fácil">Pago Fácil</option>
-				<option value="Poli">Poli</option>
-				<option value="Przelewy24">Przelewy24</option>
-				<option value="QPAY">QPAY</option>
-				<option value="Rapipago">Rapipago</option>
-				<option value="SEPA Direct Debit">SEPA Direct Debit</option>
-				<option value="Sofort">Sofort</option>
-				<option value="Stripe">Stripe</option>
-				<option value="Via Baloto">Via Baloto</option>
-				<option value="iDEAL">iDEAL</option>
+			<option value="">Select payment method...</option>
+			<?php
+			// dynamically generate payment gateways from site.json
+			if($siteconf !== null) {
+				foreach($siteconf[0]['site.payment.gateways'] as $key => $value)
+				{
+					echo "<option value=\"".$value."\">".$value."</option>";			
+				}		
+			}
+			?>
 			</select>
 			
 		<div class="ts-shop-form-field">
 		<?php
+		
+			// dynamically generate form fields from customer.json.
 			$ignore = ['customer.id','customer.diff','customer.ua','customer.signup.ua','customer.hash','customer.signup.date','customer.signup.ip'];
 			
 			if($shopconf !== null) {
@@ -64,6 +49,7 @@
 					{
 						if(!in_array($key,$ignore)) {
 							
+									
 							$key = str_replace(['.','customer'],['',''],$key);
 							$keycss = str_replace('.','-',$key);
 							if($key == 'newsletter') {
@@ -83,6 +69,7 @@
 					}
 				}
 			}
+		
 		?>
 		</div>
 			<hr />
