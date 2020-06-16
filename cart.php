cc2554fe15ed55f87e84c5aa07b73d6b6cfc6448
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2020-06-16 20:11:02 +0200

    Add files via upload

--- cart.php
+++ cart.php
@@ -12,18 +12,47 @@
 	</head>
 	<body>
 		<h1>Shopping Cart</h1>
-		
 		<div id="ts-shop-cart-form">
-		
 		<form name="ts_cart" method="post" id="ts-shop-cart-form-data">
+		<hr />
+	
+		<ul class="ts-shop-ul">
+			<li class="ts-shop-li">
+				<div class="ts-shop-ul-li-item" width="60">&#128722;</div>
+				<div class="ts-shop-ul-li-item">product.name</div>
+				<div class="ts-shop-ul-li-item">product.description</div>
+				<div class="ts-shop-ul-li-item">product.price</div>
+				<div class="ts-shop-ul-li-item">product.quantity</div>
+				<div class="ts-shop-ul-li-item">X</div>
+			</li>
+			
+			<li class="ts-shop-li">
+				<div class="ts-shop-ul-li-item" width="60">&#128722;</div>
+				<div class="ts-shop-ul-li-item">product.name</div>
+				<div class="ts-shop-ul-li-item">product.description</div>
+				<div class="ts-shop-ul-li-item">product.price</div>
+				<div class="ts-shop-ul-li-item">product.quantity</div>
+				<div class="ts-shop-ul-li-item">X</div>
+			</li>
+			
+			<li class="ts-shop-li">
+				<div class="ts-shop-ul-li-item" width="60">&#128722;</div>
+				<div class="ts-shop-ul-li-item">product.name</div>
+				<div class="ts-shop-ul-li-item">product.description</div>
+				<div class="ts-shop-ul-li-item">product.price</div>
+				<div class="ts-shop-ul-li-item">product.quantity</div>
+				<div class="ts-shop-ul-li-item">X</div>
+			</li>	
+		</ul>
+		
 		<hr />
 			<select name="payment_gateway" id="ts-form-cart-payment-gateway-select">
 			<option value="">Select payment method...</option>
 			<?php
-			// dynamically generate payment gateways from site.json
-			$siteconf = $shop->load_json("inventory/site.json");
-			$keys = 'site.payment.gateways';
-			echo $shop->gatewaylist($siteconf,$keys);
+				// dynamically generate payment gateways from site.json
+				$siteconf = $shop->load_json("inventory/site.json");
+				$keys = 'site.payment.gateways';
+				echo $shop->gatewaylist($siteconf,$keys);
 			?>
 			</select>
 			
