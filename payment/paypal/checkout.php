cc35ff64fe22e098dee73cedc944a124aaef2526
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2020-06-24 21:20:05 +0200

    Update checkout.php

--- payment/paypal/checkout.php
+++ payment/paypal/checkout.php
@@ -6,14 +6,37 @@
 	$shop  		= new Shop();
 	$shopconf 	= $shop->load_json("paypal.json");
 	
+	/*
+	* Make this dynamic, for now these are demo values.
+	*/
+	// Price of the product.
+	$item_price = 10;
+	// Handling price.
+	$handling_price = 2;
+	// Shipping price.
+	$shipping_price = 10;
+	
+	// PayPal variables: only edit this in paypal.json!
 	$paypal_domain 			= $shop->cleanInput($shopconf[0]['paypal.domain']);
 	$paypal_cancel_page 		= $shop->cleanInput($shopconf[0]['paypal.cancel.page']);
 	$paypal_return_page 		= $shop->cleanInput($shopconf[0]['paypal.return.page']);
 	$paypal_email 			= $shop->cleanInput($shopconf[0]['paypal.email']);
 	$paypal_notify_url 		= $shop->cleanInput($shopconf[0]['paypal.notify.url']);
 	$paypal_currency_code 		= $shop->cleanInput($shopconf[0]['paypal.currency.code']);
+	
 	$paypal_invoice_number 		= $shop->cleanInput($shopconf[0]['paypal.custom.field']);
+	
+	if(empty($paypal_invoice_number)) {
+		// should not be empty.
+		$paypal_invoice_number 	= 1;
+	}
+	
 	$paypal_image_url 		= $shop->cleanInput($shopconf[0]['paypal.image.url']);
+	
+	if(empty($paypal_image_url)) {
+		$paypal_image_url 	= 'http://www.paypal.com/en_US/i/btn/x-click-but01.gif';
+	}
+	
 	$paypal_no_note 		= $shop->cleanInput($shopconf[0]['paypal.no.note']);
 	$paypal_no_shipping 		= $shop->cleanInput($shopconf[0]['paypal.no.shipping']);
 	$paypal_on0 			= $shop->cleanInput($shopconf[0]['paypal.on0']);
@@ -26,6 +49,7 @@
 	/*
 	* doc: https://developer.paypal.com/docs/paypal-payments-standard/integration-guide/Appx-websitestandard-htmlvariables/#individual-items-variables
 	*/
+	
 ?>
 <!DOCTYPE html>
 <html>
@@ -40,11 +64,15 @@
 		<div id="ts-shop-form">
 			<div class="ts-shop-form-section">	
 						<label for="quantity">Product</label>
-						<input type="text" name="item_name" maxlength="127" size="20" value="A product" title="cart item, 127 chars" disabled>
+						<input type="text" name="item_name" maxlength="127" size="20" value="A product" title="cart item, 127 chars">
 						<label for="quantity">Product number</label>
-						<input type="text" name="item_number" maxlength="127" size="20" value="123456" title="track payments, 127 chars" disabled>
+						<input type="text" name="item_number" maxlength="127" size="20" value="123456" title="track payments, 127 chars">
 						<label for="quantity">Price</label>
-						<input type="text" name="item_price" maxlength="127" size="20" id="item_price" value="10" title="" disabled>
+						<input type="text" name="item_price" maxlength="127" size="20" id="item_price" value="<?=$item_price;?>" title="" disabled>
+						
+						<!-- required -->
+						<input type="hidden" name="amount" maxlength="127" size="20" id="item_price" value="<?=$item_price;?>" title="">
+						
 						<label for="quantity">Quantity</label>
 						<input type="number" name="quantity" onchange="calculateTotalPayPal(this.value);" maxlength="2" min="1" max="9999" value="1">
 			</div>
@@ -52,9 +80,11 @@
 						<input type="hidden" name="no_note" maxlength="1" min="0" max="1" value="1" title="0 or 1. 1 = no prompt">
 						<input type="hidden" name="no_shipping" maxlength="1" min="0" max="1" value="1" title="0 or 1. 0 = to add shipping address">
 						<label for="quantity">Shipping</label>
-						<input type="text" name="shipping" id="shipping" size="5" title="The item's shipping cost" value="10" disabled>
+						<?=$shipping_price;?>
+						<input type="hidden" name="shipping" id="shipping" size="5" title="The item's shipping cost" value="<?=$shipping_price;?>">
 						<label for="quantity">Handling cost</label>
-						<input type="text" name="handling" id="handling" size="5" title="handling cost" value="2" disabled>
+						<?=$handling_price;?>
+						<input type="hidden" name="handling" id="handling" size="5" title="handling cost" value="<?=$handling_price;?>">
 						<label for="quantity">Total</label>
 						<input type="text" name="amount" size="5" id="total_amount" title="total amount" value="" disabled>	
 			</div>
@@ -64,11 +94,11 @@
 				<div class="ts-shop-form-section">	
 					<input type="hidden" name="image_url" value="<?=$paypal_image_url;?>">
 					<input type="hidden" name="currency_code" value="<?=$paypal_currency_code;?>">		
-					<input type="hidden" name="business" value="<?=$paypal_domain;?>">
-					<input type="hidden" name="cancel_return" value="<?=$paypal_cancel_page;?>">
+					<input type="hidden" name="business" value="<?=$paypal_email;?>">
+					<input type="hidden" name="cancel_return" value="<?=$paypal_domain.''.$paypal_cancel_page;?>">
 					<input type="hidden" name="custom" value="<?=$paypal_currency_code;?>">
 					<input type="hidden" name="invoice" value="<?=$paypal_invoice_number;?>">
-					<input type="hidden" name="notify_url" value="<?=$paypal_notify_url;?>">
+					<input type="hidden" name="notify_url" value="<?=$paypal_domain.''.$paypal_notify_url;?>">
 					<?php
 					if($paypal_on0) {
 					?>
@@ -83,32 +113,32 @@
 					<?php
 					}
 					?>
-					<input type="hidden" name="return" value="<?=$paypal_return_page;?>">
+					<input type="hidden" name="return" value="<?=$paypal_domain.''.$paypal_return_page;?>">
 					<!-- optional -->			
 					<input type="hidden" name="cmd" value="_ext-enter">
 					<input type="hidden" name="redirect_cmd" value="_xclick">
 					
 					<label for="address1">Address</label>
-					<input type="text" name="address1" id="address1" maxlength="100" title="The first line of the customer's address (100-alphanumeric character limit).">
+					<input type="text" name="address1" id="address1" maxlength="100" value="" title="The first line of the customer's address (100-alphanumeric character limit).">
 					<label for="city">City</label>
-					<input type="text" name="city" id="city" maxlength="100" title="The city noted in the customer's address (100-alphanumeric character limit).">
+					<input type="text" name="city" id="city" maxlength="100" value="" title="The city noted in the customer's address (100-alphanumeric character limit).">
 					<label for="day_phone_a">Area code</label>
 					<input type="text" name="day_phone_a" id="day_phone_a" size="5" value="">
 					
 					<label for="state">State</label>
-					<input type="text" name="state" id="state" size="2" maxlength="2" title="The state noted in the customer's address (the official two-letter abbreviation).">
+					<input type="text" name="state" id="state" size="2" maxlength="2" value="" title="The state noted in the customer's address (the official two-letter abbreviation).">
 					<label for="zip">Zip</label>
-					<input type="text" name="zip" id="zip" size="5"  maxlength="32" title="The postal code noted in the customer's address.">
+					<input type="text" name="zip" id="zip" size="5"  maxlength="32" value="" title="The postal code noted in the customer's address.">
 				</div>
 				<div class="ts-shop-form-section">	
 					<label for="email">E-mail</label>
-					<input type="text" name="email" id="email" size="15" title="The customer's email address.">
+					<input type="text" name="email" id="email" size="15" value="" title="The customer's email address.">
 					<label for="day_phone_b">Phone</label>
-					<input type="text" name="day_phone_b" id="day_phone_b" size="7" value="">
+					<input type="text" name="day_phone_b" id="day_phone_b" size="7" value="" value="">
 					<label for="first_name">First name</label>
-					<input type="text" name="first_name" id="first_name" size="15" maxlength="32" title="The customer's first name (32-alphanumeric character limit).">
+					<input type="text" name="first_name" id="first_name" size="15" maxlength="32" value="" title="The customer's first name (32-alphanumeric character limit).">
 					<label for="last_name">Last name</label>
-					<input type="text" name="last_name" id="last_name" size="15" maxlength="64" title="The customer's last name (64-alphanumeric character limit).">
+					<input type="text" name="last_name" id="last_name" size="15" maxlength="64" value="" title="The customer's last name (64-alphanumeric character limit).">
 					<!-- 
 						input type="text" name="night_phone_a" id="night_phone_a" value="The area code of the customer's evening telephone number.">
 						<input type="text" name="night_phone_b" id="night_phone_b" value="The first three digits of the customer's evening telephone number.">
