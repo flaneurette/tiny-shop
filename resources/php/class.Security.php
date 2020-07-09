5fe94f321c3bafc5ce7f06c1bcb17fb39da6bd4c
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2020-07-09 22:12:19 +0200

    Update class.Security.php

--- resources/php/class.Security.php
+++ resources/php/class.Security.php
@@ -5,7 +5,12 @@ class Security {
 	/**
 	* Security class
 	*/
-	CONST MAXINT  = 9999999;
+	const MAXINT  			= 9999999;
+	const PHPENCODING 		= 'UTF-8';		// Characterset of PHP functions: (htmlspecialchars, htmlentities) 
+	const MINHASHBYTES		= 32; 			// Min. of bytes for secure hash.
+	const MAXHASHBYTES		= 64; 			// Max. of bytes for secure hash, more increases cost. Max. recommended: 256 bytes.
+	const MINMERSENNE		= 0xff; 		// Min. value of the Mersenne twister.
+	const MAXMERSENNE		= 0xffffffff; 	// Max. value of the Mersenne twister.
 
 	public function __construct() {
 		$securing = true;
@@ -53,12 +58,12 @@ class Security {
 				
 			break;
 			
-			case 'unicode':
-				$this->data =  preg_replace("/[^[:alnum:][:space:]]/u", '', $string);
+			case 'alphanum':
+				$this->data =  preg_replace('/[^a-zA-Z-0-9]/','', $string);
 			break;
 			
-			case 'encode':
-				$this->data =  htmlspecialchars($string,ENT_QUOTES,'UTF-8');
+			case 'field':
+				$this->data =  preg_replace('/[^A-Za-z0-9-_.@]/','', $string);
 			break;
 			
 			case 'query':
@@ -80,12 +85,86 @@ class Security {
 				$this->data = str_replace($search,$replace,$string);
 			break;
 			
+			case 'unicode':
+				$this->data =  preg_replace("/[^[:alnum:][:space:]]/u", '', $string);
+			break;
+			
+			case 'encode':
+				$this->data =  htmlspecialchars($string,ENT_QUOTES,self::PHPENCODING);
+			break;
+			
+			case 'entities':
+				$this->data =  htmlentities($string, ENT_QUOTES | ENT_HTML5, self::PHPENCODING);
+			break;
+			
+			case 'domain':
+				$this->data =  str_ireplace(array('http://','www.'),array('',''),$string);
+			break;
+			
 			default:
 			return $this->data;
 			
 			}
 		return $this->data;
 	}
+	
+	/**
+	* Allocates a pseudo random token to prevent CSRF.
+	* @return mixed boolean, void.
+	*/
+	public function getToken()
+	{
+		
+		$bytes = 0;
+		
+		if (function_exists('random_bytes')) {
+			$len   = mt_rand(self::MINHASHBYTES,self::MAXHASHBYTES);
+        		$bytes .= bin2hex(random_bytes($len));
+    		}
+		if (function_exists('openssl_random_pseudo_bytes')) {
+			$len   = mt_rand(self::MINHASHBYTES,self::MAXHASHBYTES);
+        		$bytes .= bin2hex(openssl_random_pseudo_bytes($len));
+    		}
+		
+		if(strlen($bytes) < 128) {
+			$bytes .= mt_rand(self::MINMERSENNE,self::MAXMERSENNE) . mt_rand(self::MINMERSENNE,self::MAXMERSENNE) . mt_rand(self::MINMERSENNE,self::MAXMERSENNE)
+				. mt_rand(self::MINMERSENNE,self::MAXMERSENNE) . mt_rand(self::MINMERSENNE,self::MAXMERSENNE) . mt_rand(self::MINMERSENNE,self::MAXMERSENNE) 
+				. mt_rand(self::MINMERSENNE,self::MAXMERSENNE) . mt_rand(self::MINMERSENNE,self::MAXMERSENNE) . mt_rand(self::MINMERSENNE,self::MAXMERSENNE) 
+				. mt_rand(self::MINMERSENNE,self::MAXMERSENNE) . mt_rand(self::MINMERSENNE,self::MAXMERSENNE) . mt_rand(self::MINMERSENNE,self::MAXMERSENNE); 
+		}
+		
+		$token = hash('sha512',$bytes);
+		
+		if(isset($_SESSION['token']) && $_SESSION['token'] != false) 
+		{ 
+			if(strlen($_SESSION['token']) < 128) {
+				// $this->sessionmessage('Issue found: session token is too short.'); 
+				} else {
+				return $this->sanitize($_SESSION['token'],'alphanum'); 
+			}
+		} else { 
+		return $token;
+		} 
+	} 
+	
+ 	/**
+	* Destroys the previously set token.
+	* @return mixed string.
+	*/
+	public function destroyToken()
+	{
+		try {
+			if(isset($_SESSION['token'])) {
+				$_SESSION['token'] = '';
+			}
+		} catch(Exception $e) {
+			// $this->sessionmessage('Issue: session could not be destroyed, '.$e->getMessage());
+			return false;
+		}
+		return true;
+	}
+	
+	
 }
 
 ?>
