0428e3bed15fbea84798932a4e7cfc677ecafd9d
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2020-07-11 19:52:25 +0200

    Update query.php

--- query.php
+++ query.php
@@ -1,37 +1,36 @@
 <?php
 
 session_start();
+
 /* 
  * TinyShop PHP Query script that handles all XHR queries.
 */
-include("resources/php/class.Security.php");
+
 include("resources/php/class.Session.php");
+include("resources/php/class.Security.php");
 
 $session = new Session();
 $secure = new Security();
 
-$token = $secure->getToken();
-
-$_SESSION['token'] = $token;
-
-/* 
-	todo: 
-	+ add anti-csrf
-*/
-
 if(isset($_POST['token']))  {
+	
 	// A token was provided through $_POST data. Check if it is the same as our session token.
 	if($_POST['token'] === $_SESSION['token']) {
 		// token is correct.
+		} else {
+		exit;
 	}
 }
-			
 			
 $default = null;
 
 if(isset($_GET['action'])) {
-	
 	$action = $_GET['action'];
+}
+
+if(isset($_POST['action'])) {
+	
+	$action = $_POST['action'];
 	
 	if(preg_match("/[a-zA-Z]/i",$action)) {
 		
@@ -41,8 +40,8 @@ if(isset($_GET['action'])) {
 			
 			case 'addtocart':
 			
-			$id = (int)$secure->sanitize($_GET['id'],'num');
-			$qty = (int)$secure->sanitize($_GET['qty'],'num');
+			$id  = (int)$secure->sanitize($_POST['id'],'num');
+			$qty = (int)$secure->sanitize($_POST['qty'],'num');
 			
 			$arr = [
 					'product.id' => $id,
@@ -50,32 +49,40 @@ if(isset($_GET['action'])) {
 			];
 
 			$session->addtocart($arr);
-			// echo $_SESSION['cart'][0]['product.id'];
-			// echo $_SESSION['cart'][0]['product.qty'];
+			$_SESSION['cart'] = $session->unique_array($_SESSION['cart'], 'product.id');
 			echo "Product added to cart.";
+			
 			break;
 			
 			case 'deletefromcart':
-			$id = (int)$secure->sanitize($_GET['id'],'num');
+			
+			$id = (int)$secure->sanitize($_POST['id'],'num');
+			
+			if($id) {
+				$_SESSION['cart'] = $session->deletefromcart($id);
+			}
+			
 			break;
 			
 			case 'emptycart':
-			$cartid = (int)$secure->sanitize($_GET['cartid'],'num');
+			$cartid = (int)$secure->sanitize($_POST['cartid'],'num');
+			$_SESSION['cart'] = [];
 			break;			
 			
 			case 'voucher':
-			$code = $secure->sanitize($_GET['code'],'alpha');
+			$code = $secure->sanitize($_POST['code'],'alpha');
 			break;
 			
 			case 'wishlist':
-			$product = $secure->sanitize($_GET['product'],'alpha'); 
-			$tr 	 = $secure->sanitize($_GET['tr'],'alpha'); 
+			$product = $secure->sanitize($_POST['product'],'alpha'); 
+			$tr 	 = $secure->sanitize($_POST['tr'],'alpha'); 
 			break;	
 			
 			case 'query':
 			break;
 		}
 	
+
 	} else {
 	// contains other characters.
 	echo $default;
@@ -84,3 +91,4 @@ if(isset($_GET['action'])) {
 	echo $default;
 }
 ?>
+1
