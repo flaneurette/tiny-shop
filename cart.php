1b444934a57bc6fc9df53eccdc8a62ab7ad10f10
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2020-07-12 03:23:24 +0200

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,7 +1,10 @@
 <?php
 	include("resources/php/header.inc.php");
+	include("resources/php/class.Session.php");
 	include("class.Shop.php");
+	
 	$shop = new Shop();
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,35 +19,60 @@
 		<form name="ts_cart" method="post" id="ts-shop-cart-form-data">
 		<hr />
 	
-		<ul class="ts-shop-ul">
-			<li class="ts-shop-li">
-				<div class="ts-shop-ul-li-item" width="60">&#128722;</div>
-				<div class="ts-shop-ul-li-item">product.name</div>
-				<div class="ts-shop-ul-li-item">product.description</div>
-				<div class="ts-shop-ul-li-item">product.price</div>
-				<div class="ts-shop-ul-li-item">product.quantity</div>
-				<div class="ts-shop-ul-li-item">X</div>
-			</li>
+	<ul class="ts-shop-ul">
+	
+		<li class="ts-shop-li">
+			<div class="ts-shop-ul-li-item" width="60">&#128722;</div>
+			<div class="ts-shop-ul-li-item">Product Name</div>
+			<div class="ts-shop-ul-li-item">Description</div>
+			<div class="ts-shop-ul-li-item">Price</div>
+			<div class="ts-shop-ul-li-item">Quantity</div>
+			<div class="ts-shop-ul-li-item">Delete</div>
+		</li>
 			
-			<li class="ts-shop-li">
-				<div class="ts-shop-ul-li-item" width="60">&#128722;</div>
-				<div class="ts-shop-ul-li-item">product.name</div>
-				<div class="ts-shop-ul-li-item">product.description</div>
-				<div class="ts-shop-ul-li-item">product.price</div>
-				<div class="ts-shop-ul-li-item">product.quantity</div>
-				<div class="ts-shop-ul-li-item">X</div>
-			</li>
-			
-			<li class="ts-shop-li">
-				<div class="ts-shop-ul-li-item" width="60">&#128722;</div>
-				<div class="ts-shop-ul-li-item">product.name</div>
-				<div class="ts-shop-ul-li-item">product.description</div>
-				<div class="ts-shop-ul-li-item">product.price</div>
-				<div class="ts-shop-ul-li-item">product.quantity</div>
-				<div class="ts-shop-ul-li-item">X</div>
-			</li>	
-		</ul>
+	<?php
+	
+	if(isset($_SESSION['cart'])) {
 		
+	$c = count($_SESSION['cart']);
+		
+		if(($c > 0) && ($c < 9999)) {
+			
+		$products = $shop->getproductlist();
+			
+		for($i=0; $i < $c; $i++) {
+			
+			$product = $_SESSION['cart'][$i]['product.id'];
+			$qty = $_SESSION['cart'][$i]['product.qty'];
+			$j=0;
+			
+			foreach($products as $key => $value) {
+				
+				if($products[$j][0][1] == $product) {
+					
+				//echo $products[$j][2][1]; // title
+				//echo $products[$j][3][1]; // desc
+				//echo $products[$j][18][1]; // price
+		?>
+			<li class="ts-shop-li">
+				<div class="ts-shop-ul-li-item" width="60">&#128722;</div>
+				<div class="ts-shop-ul-li-item"><?=$products[$j][2][1];?></div>
+				<div class="ts-shop-ul-li-item"><?=$products[$j][3][1];?></div>
+				<div class="ts-shop-ul-li-item"><?=$products[$j][18][1];?></div>
+				<div class="ts-shop-ul-li-item"><input type="number" size="1" max="9999" value="<?=$qty;?>"></div>
+				<div class="ts-shop-ul-li-item"><input type="button" value="X"></div>
+			</li>
+		<?php
+				 }
+				 $j++;
+				}
+			}
+			echo "</ul>";
+		}
+	} else {
+		echo "<h3>Cart is empty.</h3>";
+	} 
+		?>
 		<hr />
 			<select name="payment_gateway" id="ts-form-cart-payment-gateway-select">
 			<option value="">Select payment method...</option>
