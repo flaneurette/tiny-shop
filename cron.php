88bbb6046a5e6614f48107a7a9e57ccd635009c9
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2020-09-23 03:53:20 +0200

    Update cron.php

--- cron.php
+++ cron.php
@@ -1,5 +1,11 @@
 <?php
-// cron.php
+
+// Cron.php
+
+/* 
+  Add a crontab through a linux terminal (sudo crontab -e) and link to /var/www/html/cron.php to empty the /session/ folder.
+  !! this file is still experimental, and might not work on all instances. !!
+*/
 
 system("rm -f -R ../session/");
 system("mkdir ../session/");
